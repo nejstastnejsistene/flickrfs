a4a567882b91dee6aa11a0fabb866eda109bba77
brc <brett@cooleyweb.org>
2013-09-07 18:16:54 +0000

    Fixed tabs -> spaces

--- up_down.py
+++ up_down.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
+
 # up_down.py
-# uploads images to flickr and 
-# downloads original image links 
+# uploads images to flickr and
+# downloads original image links
 import flickrapi
 import sys
 
-# upload() 
+# upload()
 # upload image
 def upload(api_key, api_secret, tok, img):
-  flickr = flickrapi.FlickrAPI(api_key, api_secret, token=tok)
-  #flickr.upload(img, callback=show_progress)
-  flickr.upload(img)
+    flickr = flickrapi.FlickrAPI(api_key, api_secret, token=tok)
+    #flickr.upload(img, callback=show_progress)
+    flickr.upload(img)
 
-''' # show_progress doesn't seem to catch the callback
-def show_progress(progress, done):
-  if done:
+    ''' # show_progress doesn't seem to catch the callback
+    def show_progress(progress, done):
+    if done:
     print "Done!"
-  else:
+    else:
     print "At %s%%" % progress
     '''
 
@@ -24,38 +25,39 @@ def show_progress(progress, done):
 # prints a url of the original images
 # on the authenticated users flickr
 def search(api_key, api_secret):
-  flickr = flickrapi.FlickrAPI(api_key, api_secret)
-  uid = 'me' # key to indicate the authenticated user
-  photos = flickr.photos_search(user_id=uid, extras='original_format')
+    flickr = flickrapi.FlickrAPI(api_key, api_secret)
+    uid = 'me' # key to indicate the authenticated user
+    photos = flickr.photos_search(user_id=uid, extras='original_format')
 
-  for photo in photos[0]:
-    # Build the url, for details see 
-    # http://www.flickr.com/services/api/misc.urls.html
-    url = ('http://farm' +
-	   photo.attrib['farm'] + '.staticflickr.com/' +
-	   photo.attrib['server'] + '/' +
-	   photo.attrib['id'] + '_' + 
-	   photo.attrib['originalsecret'] + '_o.png')
-    print url, photo.attrib['title']
+    for photo in photos[0]:
+        # Build the url, for details see
+        # http://www.flickr.com/services/api/misc.urls.html
+        url = ('http://farm' +
+           photo.attrib['farm'] + '.staticflickr.com/' +
+           photo.attrib['server'] + '/' +
+           photo.attrib['id'] + '_' +
+           photo.attrib['originalsecret'] + '_o.png')
+        print url, photo.attrib['title']
 
 # print_usage()
 # prints command line usage
 def print_usage():
-  print 'usage: up_down.py [img]'
+    print 'usage: up_down.py [img]'
 
 def main():
-  api_key = # API_KEY GOES HERE
-  api_secret = # API_SECRET GOES HERE
-  tok = # TOKEN GOES HERE
+    api_key = # API_KEY GOES HERE
+    api_secret = # API_SECRET GOES HERE
+    tok = # TOKEN GOES HERE
 
-  if len(sys.argv) != 2:
-    print_usage(); sys.exit(1)
+    if len(sys.argv) != 2:
+        print_usage()
+        sys.exit(1)
 
-  img = sys.argv[1]
+    img = sys.argv[1]
 
-  upload(api_key, api_secret, tok, img)
-  search(api_key, api_secret)
+    upload(api_key, api_secret, tok, img)
+    search(api_key, api_secret)
 
 
 if __name__ == '__main__':
-  main()
+    main()
