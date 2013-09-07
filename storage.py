510367fb026a32928a1625cf45cebb95faae3396
brc <brett@cooleyweb.org>
2013-09-07 20:33:04 +0000

    Fixed storage tabs/comments

--- storage.py
+++ storage.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # storage.py
 
+import os
 import sys
 import json
 import redis
-import os
 import requests
 
 from flickrapi import FlickrAPI
@@ -12,46 +12,44 @@ from flickrapi import FlickrAPI
 class FStore(object):
 
     def __init__(self, token):
-	api_key = os.environ.get('FLICKR_API_KEY')
-	api_secret = os.environ.get('FLICKR_SECRET')
-	assert api_key != None and api_secret != None
+        api_key = os.environ.get('FLICKR_API_KEY')
+        api_secret = os.environ.get('FLICKR_SECRET')
+        assert api_key != None and api_secret != None
         self.flickr = FlickrAPI(api_key, api_secret, token=token)
 
-    # upload()
-    # takes a list of images and uploads to flickr
     def _upload(self, img_list):
-	assert isinstance(img_list, list)
-	for img in img_list: 
-	    print 'uploading ' + img
-	    self.flickr.upload(img)
+        '''Takes a list of images and uploads to flickr'''
+        assert isinstance(img_list, list)
+        for img in img_list:
+            # print 'uploading ' + img
+            self.flickr.upload(img)
 
-    # download()
-    # takes a list of flickr photo ids and downloads them
-    # returns the absolute filepath
     def _download(self, photo_ids):
-	assert isinstance(photo_ids, list)
-	flist = []
-	for pid in photo_ids:
-	    sizes = self.flickr.photos_getSizes(photo_id=pid)
-	    img = requests.get(sizes[0][-1].attrib['source'])
+        '''Takes a list of flickr photo_ids and downloads them
 
-	    # use the photo id as the file name
-	    with open(pid, 'w+') as f:
-		f.write(img.content)
+        Returns the absolute filepath'''
+        assert isinstance(photo_ids, list)
+        flist = []
+        for pid in photo_ids:
+            sizes = self.flickr.photos_getSizes(photo_id=pid)
+            img = requests.get(sizes[0][-1].attrib['source'])
 
-	    # save the file path in our list
-	    flist.append( os.path.abspath(pid) )
-	
-	# return the list of fully-qualified pathnames
-	return flist
+            # use the photo id as the file name
+            with open(pid, 'w+') as f:
+            f.write(img.content)
 
-    # delete()
-    # takes a list of flickr photo ids and deletes them
+            # save the file path in our list
+            flist.append(os.path.abspath(pid))
+
+        # return the list of fully-qualified pathnames
+        return flist
+
     def _delete(self, photo_ids):
-	assert isinstance(photo_ids, list)
-	for pid in photo_ids:
-	    print 'deleting ' + pid
-	    self.flickr.photos_delete(photo_id=pid)
+        '''Takes a list of flickr photo_ids and deletes them'''
+        assert isinstance(photo_ids, list)
+        for pid in photo_ids:
+            # print 'deleting ' + pid
+            self.flickr.photos_delete(photo_id=pid)
 
 
 def print_usage():
@@ -62,7 +60,7 @@ def main():
     ffs = FStore(token)
 
 
-    ########## upload photos ##########
+    ########## delete photos ##########
     photo_ids = ['9696283248', '9696282758']
     ffs._delete(photo_ids)
 
@@ -75,10 +73,10 @@ def main():
     '''
     ########## upload photos ##########
     img_list  = ['images/flyingBrearJackassKills.png', 'images/sodaEffects.png',
-	    'images/franklinOnLife.png', 'images/stallman.png',
-	    'images/hardwarePoster.png', 'images/ubuntuJoke.png',
-	    'images/linuxFlowchart.png', 'images/watermelonMurder.png',
-	    'images/plagueDoctor.png']
+        'images/franklinOnLife.png', 'images/stallman.png',
+        'images/hardwarePoster.png', 'images/ubuntuJoke.png',
+        'images/linuxFlowchart.png', 'images/watermelonMurder.png',
+        'images/plagueDoctor.png']
 
     ffs._upload(img_list)
     '''
