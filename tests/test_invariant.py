75c971bdfb353176b3001a4b4736d9216d2f8a25
Peter Johnson <pajohnson@email.wm.edu>
2013-09-11 13:14:36 -0400

    Beefed up testing to enclude encoding with and without chunking, encode+decode with and without chunking, and round trip with and without chunking.

--- tests/test_invariant.py
+++ tests/test_invariant.py
@@ -2,14 +2,56 @@ import os
 from StringIO import StringIO
 from flickrfs import *
 
-def test_without_chunking(filename='README.md'):
+def test_encode_without_chunking(filename='README.md'):
+    filenames = list(encodepng(filename))
+    try:
+        assert len(filenames) == 1
+    finally:
+        for tmpfile in filenames:
+            os.unlink(tmpfile)
+
+def test_encode_with_chunking(filename='README.md'):
+    with open(filename) as f:
+        original_data = f.read()
+    filenames = list(encodepng(filename, chunk_size=1))
+    try:
+        assert len(filenames) == len(original_data)
+        assert os.path.basename(filenames[0]) == filename + '.png'
+        for i, fname in enumerate(filenames[1:], 2):
+            ext = '.part' + str(i) + '.png'
+            assert os.path.basename(fname) == filename + ext
+    finally:
+        for tmpfile in filenames:
+            os.unlink(tmpfile)
+
+def test_encode_and_decode_without_chunking(filename='README.md'):
+    with open(filename) as f:
+        original_data = f.read()
+    filenames = list(encodepng(filename, chunk_size=1))
+    s = StringIO()
+    decodepngs(filenames, outfile=s)
+    assert s.getvalue() == original_data
+    for tmpfile in filenames:
+        assert not os.path.exists(tmpfile)
+
+def test_encode_and_decode_with_chunking(filename='README.md'):
+    with open(filename) as f:
+        original_data = f.read()
+    filenames = list(encodepng(filename, chunk_size=1))
+    s = StringIO()
+    decodepngs(filenames, outfile=s)
+    assert s.getvalue() == original_data
+    for tmpfile in filenames:
+        assert not os.path.exists(tmpfile)
+
+def test_roundtrip_without_chunking(filename='README.md'):
     with open(filename) as f:
         original_data = f.read()
     s = StringIO()
     decodepngs(flickrdownload(flickrupload(encodepng(filename))), outfile=s)
     assert s.getvalue()[:len(original_data)] == original_data
 
-def test_with_chunking(filename='README.md'):
+def test_roundtrip_with_chunking(filename='README.md'):
     with open(filename) as f:
         original_data = f.read()
     # Round down the image size to the next lower perfect square.
